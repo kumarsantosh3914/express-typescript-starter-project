57bf363afa1db763e5e5da88e4147d47c1c5bfe5
Santosh Kumar <santoshkumar15novmth@gmail.com>
2025-04-12 00:44:13 +0530

    added server config, routing and controller layer

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import { Express } from 'express';
+import { serverConfig } from './config';
+import v1Router from './routers/v1/index.router';
+import v2Router from './routers/v2/index.router';
 
 const app: Express = express();
 
-const PORT: number = 3001;
+// Regestering all the routers and their corresponding routes with out app server object.
+app.use('/api/v1', v1Router);
+app.use('/api/v2', v2Router);
 
-app.get('/ping', (req: Request, res: Response) => {
-    res.send('Pong!');
-})
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(serverConfig.PORT, () => {
+    console.log(`Server is running on http://localhost:${serverConfig.PORT}`);
 })
